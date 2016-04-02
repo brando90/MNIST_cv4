193e21749979f8d9c335d2541eb868d6ba836b69
Brando <brando90@mit.edu>
2016-04-01 23:18:49 -0400

    missing results folder

---
+++ results/r_30mar_ht1_HBF1/time_duration_om_id1.m
@@ -0,0 +1,7 @@
+task_id=1;
+secs=1.329791e+01;
+minutes=2.216318e-01;
+hours=3.693863e-03;
+iterations=5;
+center=10;
+data_set= '../../hbf_research_data/data_MNIST_0.7_0.15_0.15_49000_10500_10500.mat' ;
---
+++ results/r_30mar_ht1_HBF1/time_duration_om_id2.m
@@ -0,0 +1,7 @@
+task_id=2;
+secs=2.185715e+01;
+minutes=3.642858e-01;
+hours=6.071430e-03;
+iterations=5;
+center=250;
+data_set= '../../hbf_research_data/data_MNIST_0.7_0.15_0.15_49000_10500_10500.mat' ;
