b2dabbdecaad169321f19202d4a04da2d6ea9453
Brando <brando90@mit.edu>
2016-04-05 16:09:41 -0400

    results

---
+++ results/r_4apr_omt1_HBF1/results_om_id2.m
@@ -0,0 +1,6 @@
+task_id=2;
+center=70;
+test_error_H_mdl=8.715165e+00;
+train_error_H_mdl=8.940505e+00;
+test_error_kernel_mdl=8.691958e+00;
+train_error_kernel_mdl=8.916611e+00;
---
+++ results/r_4apr_omt1_HBF1/time_duration_om_id2.m
@@ -0,0 +1,7 @@
+task_id=2;
+secs=9.931157e+04;
+minutes=1.655193e+03;
+hours=2.758655e+01;
+iterations=5000;
+center=70;
+data_set= '../../hbf_research_data/data_MNIST_0.7_0.15_0.15_49000_10500_10500.mat' ;
